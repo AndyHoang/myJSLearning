53c78649013562340d3c2ec02255a5355d4be5c5
AndyHoang <ngocdunghoangdl@gmail.com>
2015-03-07 16:30:23 +0700

    class starter

---
+++ class_module.js
@@ -0,0 +1,61 @@
+function inherit(o) {
+  if (o == null) throw TypeError();
+  if (Object.create) return Object.create(o);
+  var t = typeof o
+  if (t !== 'object' && t !== 'function') throw TypeError();
+
+  function f() {}
+  f.prototype = o;
+  return new f();
+}
+
+function range(from, to) {
+  var r = inherit(range.methods)
+  r.from = from;
+  r.to = to;
+  return r;
+}
+range.methods = {
+  includes: function(x) {
+    return (x >= this.from && x <= this.to)
+  },
+  foreach: function(f) {
+    for (var a = Math.ceil(this.from); a <= this.to; a++) {
+      f(a);
+    }
+  },
+  toString: function() {
+    return '( ' + this.from + ' ... ' + this.to + ' )'
+  }
+
+};
+
+var testrange = range(2, 5);
+testrange.foreach(console.log);
+console.log(testrange.includes(3));
+console.log(testrange.toString());
+
+
+function Range(from, to) {
+  this.from = from;
+  this.to = to;
+
+}
+
+Range.prototype = {
+  includes: function(x) {
+    return (x >= this.from && x <= this.to)
+  },
+  foreach: function(f) {
+    for (var a = Math.ceil(this.from); a <= this.to; a++) {
+      f(a);
+    }
+  },
+  toString: function() {
+    return '( ' + this.from + ' ... ' + this.to + ' )'
+  }
+}
+
+var r = new Range(10, 20);
+console.log(r.includes(3));
+console.log(r.toString());
