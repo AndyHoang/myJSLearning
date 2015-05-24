e4b92322a4b5e1051386149dc6880303c02b98ef
AndyHoang <ngocdunghoangdl@gmail.com>
2015-05-24 09:55:45 +0700

    mimic "new" keyword

--- __proto__.js
+++ __proto__.js
@@ -14,8 +14,39 @@ console.log(andy.getInfo());
 var hoang = {
   name: 'Hoang',
   age: '30',
-  __proto__: andy
+  getInfo: function() {
+    return 'Name :' + this.name + ' age: ' + this.age;
+  },
+  __proto__: Customer.prototype
 }
 
 console.log(hoang.getInfo());
 console.log(hoang.sex);
+
+var literalCustomer = {};
+literalCustomer.constructor = Customer;
+literalCustomer.name = 'liter';
+literalCustomer.age = 15;
+literalCustomer.getInfo = function() {
+  return 'Name :' + this.name + ' age: ' + this.age;
+}
+
+console.log(literalCustomer.getInfo());
+
+console.log(literalCustomer instanceof Customer);
+console.log(literalCustomer.constructor);
+console.log(hoang instanceof Customer); //<= hoang is a Customer?
+
+function instance_of(V, F) {
+  var O = F.prototype;
+  V = V.__proto__;
+  while (true) {
+    if (V === null)
+      return false;
+    if (O === V)
+      return true;
+    V = V.__proto__;
+  }
+}
+
+// how __proto__ work in new Object?
